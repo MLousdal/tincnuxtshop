e1cf704dc7d8eb8f303c32696cca5102679f90f2
Mads Lousdal <74186479+MLousdal@users.noreply.github.com>
2022-06-03 10:00:53 +0200

    Img on product

---
+++ components/FavoriteBtn.vue
@@ -0,0 +1,19 @@
+<script setup>
+// defineProps({
+//   favorite: Boolean,
+// });
+
+const state = reactive({ wishlisted: false });
+</script>
+
+<template>
+  <input
+    type="image"
+    :src="
+      state.wishlisted ? '/icons/like-filled.svg' : '/icons/like-outline.svg'
+    "
+    alt=""
+    class="favoriteBtn"
+    @click="state.wishlisted = !state.wishlisted"
+  />
+</template>
--- pages/products/[product].vue
+++ pages/products/[product].vue
@@ -1,5 +1,5 @@
 <template :key="handle">
-  <section class="container mx-auto">
+  <section class="product-page-wrapper">
     <div
       v-if="product"
       class="grid grid-cols-1 mx-auto my-4 md:grid-cols-[auto_minmax(300px,_1fr)] md:gap-8 md:my-8"
@@ -10,7 +10,52 @@
           <Meta name="description" :content="product.description" />
         </Head>
       </Html>
-      <ProductImage
+      <Breadcrumbs />
+      <section class="product-page">
+        <ImageCarousel :images="product.images?.edges ?? ''"></ImageCarousel>
+        <section>
+          <header class="product-page-header">
+            <h1>{{ product.title }}</h1>
+            <div class="flex flex-between flex-align-center">
+              <p class="text-5 bold text-serif">
+                {{ product.priceRange.minVariantPrice.amount }} kr.
+              </p>
+              <div class="product-functions">
+                <FavoriteBtn></FavoriteBtn>
+                <button>Tilføj til kurv</button>
+              </div>
+            </div>
+          </header>
+          <main>
+            <header>
+              <h2>Beskrivelse</h2>
+            </header>
+            <div
+              v-html="product.descriptionHtml"
+              class="flex flex-column flex-gap-2"
+            ></div>
+            <ul class="product-page-list">
+              <li>Zero waste</li>
+              <li>Vegansk / vegan</li>
+            </ul>
+            <div class="flex flex-align-center flex-gap-1">
+              <img src="/images/svanemærke.png" alt="" class="icon-l" />
+              <img src="/images/nøglehulmærke.png" alt="" class="icon-l" />
+            </div>
+            <div class="flex flex-between flex-align-center">
+              <div class="flex flex-column flex-gap-2">
+                <p>Anmeldelser (0)</p>
+                <Stars :rating="0"></Stars>
+              </div>
+              <p class="flex flex-gap-1">
+                <span class="text-nowrap">læs anmeldelser</span>
+                <img src="/icons/arrow.svg" alt="" class="icon" />
+              </p>
+            </div>
+          </main>
+        </section>
+      </section>
+      <!-- <ProductImage
         :alt="product.handle"
         :src="product.images?.edges[0]?.node?.url ?? ''"
         :width="product.images?.edges[0]?.node?.width ?? ''"
@@ -31,8 +76,8 @@
         />
         <ProductVariants label="Select option" :variants="variants" />
         <ProductAddToCart />
-        <ProductDescription :description="product.descriptionHtml" />
-      </div>
+        <ProductDescription :description="product.descriptionHtml" /> 
+      </div> -->
     </div>
     <div v-else></div>
     <div v-if="error">Error</div>
@@ -44,6 +89,7 @@ import { useQuery, useResult } from "@vue/apollo-composable";
 import { getSrcset } from "~/utils/images";
 import { productByHandle } from "~/apollo/queries/productByHandle";
 import { productVariantsByHandle } from "~/apollo/queries/productVariantsByHandle";
+import Breadcrumbs from "~~/components/Breadcrumbs.vue";
 
 const route = useRoute();
 const handle = route.params.product;
